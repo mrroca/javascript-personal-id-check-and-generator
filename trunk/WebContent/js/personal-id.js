c23b33a01959703bb713acd7df1f1546916b3d45
jan.sch.hansen <jan.sch.hansen@45a318a0-b0a7-b2d6-d31d-d808f67c683e>
2011-03-15 13:19:42 +0000

    Fix errors reported by jslint.

--- trunk/WebContent/js/personal-id.js
+++ trunk/WebContent/js/personal-id.js
@@ -1,18 +1,21 @@
 /*!
  * Personal id JavaScript Library
  *
- * Created by Jan Schrøder Hansen
+ * Created by Jan Schroeder Hansen
  *
  */
 function calculateModules11(id, to) {
 	var multiply = [4, 3, 2, 7, 6, 5, 4, 3, 2, 1];
 	var sum = 0;
 	var pos = 0;
+	var i;
 	for (i = 0; i < to; i++) {
 		var x = id.substring(pos, pos + 1);
-		var intValue = parseInt(x);
+		var intValue = parseInt(x, 10);
 		sum += intValue * multiply[i];
-		if (i == 5) pos++; //Ignore dash
+		if (i === 5) {
+			pos++; //Ignore dash
+		}
 		pos++;
 	}
 	return sum;
@@ -20,26 +23,31 @@ function calculateModules11(id, to) {
 
 function calculateSECheckSum(id) {
 	var sum = 0;
-	for (var pos = 0; pos < 10; pos++) {
+	var pos;
+	for (pos = 0; pos < 10; pos++) {
 		switch (pos) {
 			case 0:
 			case 2:
 			case 4:
 			case 7:
 			case 9:
-				var intValue = 2 * parseInt(id.substring(pos, pos + 1));
-				if (intValue > 9) intValue = 1 + (intValue - 10);
+				var intValue = 2 * parseInt(id.substring(pos, pos + 1), 10);
+				if (intValue > 9) {
+					intValue = 1 + (intValue - 10);
+				}
 				sum += intValue;
 				break;
 			case 6: //Ignore dash
 				break;
 			default:
-				sum += parseInt(id.substring(pos, pos + 1));
+				sum += parseInt(id.substring(pos, pos + 1), 10);
 				break;
 		}
 	}
     var checksum = 10 - (sum % 10);
-    if (checksum == 10) checksum = 0;
+    if (checksum === 10) {
+		checksum = 0;
+	}
 	return checksum;
 }
 
@@ -64,7 +72,7 @@ function TestDKPersonalId(id) {
 				returnHtml += "<li>Modulus 11 fejl</li>";
 			}
 			var birhtdate = this.getBirthDate();
-			if (birhtdate == null) {
+			if (birhtdate === null) {
 				errorFound = true;
 				returnHtml += "<li>Ikke gyldig fødselsdag</li>";
 			}
@@ -104,14 +112,14 @@ function TestDKPersonalId(id) {
 	
 	this.modulus11Validation = function() {
 		var modulus11Ok = false;
-		if (id.length == 11) {
+		if (id.length === 11) {
 			if (this.noModulus11Validation()) {
 				return true;
 			}
 			else {
 				var sum = calculateModules11(id, 10);
-	            if (sum % 11 == 0) {
-	            	modulus11Ok = true;
+	            if (sum % 11 === 0) {
+					modulus11Ok = true;
 	            }
 			}
 		}
@@ -121,13 +129,13 @@ function TestDKPersonalId(id) {
 	this.noModulus11Validation = function() {
 		var birthdate = this.getBirthDate();
 		var gender = this.getGender();
-		if (birthdate != null) {
+		if (birthdate !== null) {
 			var noModulusOn01011965 = $.datepicker.parseDate('ddmmyy', '01011965'); 
-			if (birthdate.getTime() == noModulusOn01011965.getTime() && gender === "MALE") {
+			if (birthdate.getTime() === noModulusOn01011965.getTime() && gender === "MALE") {
 				return true;
 			}
 			var noModulusOn01011966 = $.datepicker.parseDate('ddmmyy', '01011966'); 
-			if (birthdate.getTime() == noModulusOn01011966.getTime() && gender === "MALE") {
+			if (birthdate.getTime() === noModulusOn01011966.getTime() && gender === "MALE") {
 				return true;
 			}
 		}
@@ -135,11 +143,10 @@ function TestDKPersonalId(id) {
 	};
 	
 	this.getFullYear = function() {
-		var year = parseInt(id.substring(4, 6));
-		var digit8 = parseInt(id.substring(7, 8));
+		var year = parseInt(id.substring(4, 6), 10);
+		var digit8 = parseInt(id.substring(7, 8), 10);
 		var fullYear = 0;
-        switch (digit8)
-        {
+        switch (digit8) {
             case 0:
             case 1:
             case 2:
@@ -147,12 +154,10 @@ function TestDKPersonalId(id) {
                 fullYear = 1900 + year;                       
                 break;
             case 4:
-                if (year < 37)
-                {
+                if (year < 37) {
                     fullYear = 2000 + year;
                 }
-                else
-                {
+                else {
                     fullYear = 1900 + year;
                 }
                 break;
@@ -160,22 +165,18 @@ function TestDKPersonalId(id) {
             case 6:
             case 7:
             case 8:
-                if (year > 57)
-                {
+                if (year > 57) {
                     fullYear = 1800 + year;
                 }
-                else
-                {
+                else {
                     fullYear = 2000 + year;
                 }
                 break;
             case 9:
-                if (year > 36)
-                {
+                if (year > 36) {
                     fullYear = 1900 + year;
                 }
-                else
-                {
+                else {
                     fullYear = 2000 + year;
                 }
                 break;
@@ -194,7 +195,7 @@ function TestDKPersonalId(id) {
 	};
 	
 	this.getGender = function() {
-		var digit11 = parseInt(id.substring(10, 11));
+		var digit11 = parseInt(id.substring(10, 11), 10);
 		var x = digit11 % 2;
 		if (x === 0) {
 			return "FEMALE";
@@ -216,42 +217,56 @@ function GenerateDKPersonalIds(gender, birthdate) {
 	this.generate = function() {
 		var fixpart = "";
 		var day = birthdate.getDate();
-		if (day < 10) fixpart += "0";
+		if (day < 10) {
+			fixpart += "0";
+		}
 		fixpart += day;
 		var month = birthdate.getMonth() + 1;
-		if (month < 10) fixpart += "0";
+		if (month < 10) {
+			fixpart += "0";
+		}
 		fixpart += month;
-		var fullYear = birthdate.getFullYear();
-		var strYear = "" + fullYear;
+		var fullYear = birthdate.getFullYear().toString();
+		var strYear = fullYear;
 		fixpart += strYear.substring(2, 4);
 		fixpart += "-"; 
 		
 		var result = "";
 		var valid8Digits = this.getValid8Digits(fullYear);
 		var counter = 0;
-		for(var next8Digit = 0; next8Digit < valid8Digits.length; next8Digit++) {
-			for (var nextSerialNumber = 0; nextSerialNumber <= 99; nextSerialNumber++) {
+		var next8Digit;
+		var nextSerialNumber;
+		for(next8Digit = 0; next8Digit < valid8Digits.length; next8Digit++) {
+			for (nextSerialNumber = 0; nextSerialNumber <= 99; nextSerialNumber++) {
 				var nextId = fixpart;
 				nextId += valid8Digits[next8Digit];
-				if (nextSerialNumber < 10) nextId += "0";
+				if (nextSerialNumber < 10) {
+					nextId += "0";
+				}
 				nextId += nextSerialNumber;
 				var sum = calculateModules11(nextId, 9);
 	            var remainder = sum % 11;
 	            var modulus11 = 0;
-	            if (remainder != 0) modulus11 = 11 - remainder;
+	            if (remainder !== 0) {
+					modulus11 = 11 - remainder;
+				}
 				if (modulus11 < 10) {
 					nextId += modulus11;
-					if (modulus11 % 2 == 0 && gender === "FEMALE") {
-						if (counter > 0) result += ", ";
+					if (modulus11 % 2 === 0 && gender === "FEMALE") {
+						if (counter > 0) {
+							result += ", ";
+						}
 						result += "<nobr>" + nextId + "</nobr>";
 						counter++;
 					}
-					else if (modulus11 % 2 != 0 && gender === "MALE") {
-						if (counter > 0) result += ", ";
+					else if (modulus11 % 2 !== 0 && gender === "MALE") {
+						if (counter > 0) {
+							result += ", ";
+						}
 						result += "<nobr>" + nextId + "</nobr>";
 						counter++;
 					}
- 				}
+				}
 			}
 		}
 		result += "<br /><br /><b>Antal: " + counter + "</b>";
@@ -264,41 +279,37 @@ function GenerateDKPersonalIds(gender, birthdate) {
 		var valid8Digits = [];
 		var nextElement = 0;
 
-        if (century == 19)
-        {
+        if (century === 19) {
             valid8Digits[nextElement++] = 0;
             valid8Digits[nextElement++] = 1;
             valid8Digits[nextElement++] = 2;
             valid8Digits[nextElement++] = 3;
             if (year >= 37)
             {
-            	valid8Digits[nextElement++] = 4;
-            	valid8Digits[nextElement++] = 9;
+				valid8Digits[nextElement++] = 4;
+				valid8Digits[nextElement++] = 9;
             }
         }
-        else if (century == 20)
-        {
-            if (year < 37)
-            {
-            	valid8Digits[nextElement++] = 4;
+        else if (century === 20) {
+            if (year < 37) {
+				valid8Digits[nextElement++] = 4;
             }
-            if (year <= 57)
-            {
-            	valid8Digits[nextElement++] = 5;
-            	valid8Digits[nextElement++] = 6;
-            	valid8Digits[nextElement++] = 7;
-            	valid8Digits[nextElement++] = 8;
+            if (year <= 57) {
+				valid8Digits[nextElement++] = 5;
+				valid8Digits[nextElement++] = 6;
+				valid8Digits[nextElement++] = 7;
+				valid8Digits[nextElement++] = 8;
             }
-            if (year <= 36) valid8Digits[nextElement++] = 9;
+            if (year <= 36) {
+				valid8Digits[nextElement++] = 9;
+			}
         }
-        else if (century == 18)
-        {
-            if (year > 57)
-            {
-            	valid8Digits[nextElement++] = 5;
-            	valid8Digits[nextElement++] = 6;
-            	valid8Digits[nextElement++] = 7;
-            	valid8Digits[nextElement++] = 8;
+        else if (century === 18) {
+            if (year > 57) {
+				valid8Digits[nextElement++] = 5;
+				valid8Digits[nextElement++] = 6;
+				valid8Digits[nextElement++] = 7;
+				valid8Digits[nextElement++] = 8;
             }
         }
         return valid8Digits;
@@ -327,8 +338,8 @@ function TestSEPersonalId(id) {
 				returnHtml += "<li>Checksum fejl</li>";
 			}
 			var birhtdate = this.getBirthDate();
-			if (birhtdate == null) {
-				errorFound = true;
+			if (birhtdate === null) {
+				errorsFound = true;
 				returnHtml += "<li>Ikke gyldig fødselsdag</li>";
 			}
 		}
@@ -350,7 +361,6 @@ function TestSEPersonalId(id) {
 			okReturnHtml += "</ul>";
 			return okReturnHtml;
 		}
-		return returnHtml; 
 	};
 	
 	this.formatValidation = function() {
@@ -365,11 +375,11 @@ function TestSEPersonalId(id) {
 	
 	this.checksumValidation = function() {
 		var checksumOk = false;
-		if (id.length == 11) {
+		if (id.length === 11) {
 			var calcCheckSum = calculateSECheckSum(id);
-			var checkSum = parseInt(id.substring(10, 11));
-            if (calcCheckSum == checkSum) {
-            	checksumOk = true;
+			var checkSum = parseInt(id.substring(10, 11), 10);
+            if (calcCheckSum === checkSum) {
+				checksumOk = true;
             }
 		}
 		return checksumOk;
@@ -390,23 +400,18 @@ function TestSEPersonalId(id) {
 		var year = now.getFullYear();
         var firstPart = Math.floor(year / 100); 
         var secondPart = year - firstPart * 100;
-        var yearPartFromPersonalIdentityNumber = parseInt(id.substring(4, 6));
+        var yearPartFromPersonalIdentityNumber = parseInt(id.substring(4, 6), 10);
         var dash = id.substring(6, 7);
-        if (yearPartFromPersonalIdentityNumber <= secondPart)
-        {
-            if (dash == '+') //Person over 100 years old
-            {
+        if (yearPartFromPersonalIdentityNumber <= secondPart) {
+            if (dash === '+') { //Person over 100 years old
                 firstPart--;
             }                
         }
-        else
-        {
-            if (dash == '+') //Person over 100 years old
-            {
+        else {
+            if (dash === '+') { //Person over 100 years old
                 firstPart = firstPart - 2;
             }
-            else
-            {
+            else {
                 firstPart--;
             }
         }
@@ -414,7 +419,7 @@ function TestSEPersonalId(id) {
 	};
 	
 	this.getGender = function() {
-		var digit10 = parseInt(id.substring(9, 10));
+		var digit10 = parseInt(id.substring(9, 10), 10);
 		var x = digit10 % 2;
 		if (x === 0) {
 			return "FEMALE";
@@ -442,13 +447,17 @@ function GenerateSEPersonalIds(gender, birthdate) {
 		}
 		var fixpart = "";
 		var day = birthdate.getDate();
-		if (day < 10) fixpart += "0";
+		if (day < 10) {
+			fixpart += "0";
+		}
 		fixpart += day;
 		var month = birthdate.getMonth() + 1;
-		if (month < 10) fixpart += "0";
+		if (month < 10) {
+			fixpart += "0";
+		}
 		fixpart += month;
-		var fullYear = birthdate.getFullYear();
-		var strYear = "" + fullYear;
+		var fullYear = birthdate.getFullYear().toString();
+		var strYear = fullYear;
 		fixpart += strYear.substring(2, 4);
 		if (age < 100) {
 			fixpart += "-";
@@ -458,16 +467,21 @@ function GenerateSEPersonalIds(gender, birthdate) {
 		}
 		var result = "";
         var startFrom = 0;
-        if (gender == "MALE") {
+        if (gender === "MALE") {
             startFrom = 1;
         }
         var counter = 0;
-        for (var i = startFrom; i < 1000; i = i + 2) {
-        	var nextId = fixpart;
-            if (i < 100) nextId += "0";
-            if (i < 10) nextId += "0";
-            nextId += "" + i;
-            nextId += "" + calculateSECheckSum(nextId);
+		var i;
+        for (i = startFrom; i < 1000; i = i + 2) {
+			var nextId = fixpart;
+            if (i < 100) {
+				nextId += "0";
+			}
+            if (i < 10) {
+				nextId += "0";
+			}
+            nextId += i;
+            nextId += calculateSECheckSum(nextId);
 			if (counter > 0) {
 				result += ", ";
 			}
@@ -485,11 +499,9 @@ function testPersonalId(id, country) {
 		case "dk":
 			var dkTester = new TestDKPersonalId(id);
 			return dkTester.getResultAsHtml();
-			break;
 		case "se":
 			var seTester = new TestSEPersonalId(id);
 			return seTester.getResultAsHtml();
-			break;
 		default:
 			return "Kun danske og svenske person id'er kan testes.";
 	}
@@ -500,11 +512,9 @@ function generatePersonalIds(country, gender, birthdate) {
 	case "dk":
 		var dkGenerator = new GenerateDKPersonalIds(gender, birthdate);
 		return dkGenerator.getResultAsHtml();
-		break;
 	case "se":
 		var seGenerator = new GenerateSEPersonalIds(gender, birthdate);
 		return seGenerator.getResultAsHtml();
-		break;
 	default:
 		return "Kun danske og svenske person id'er kan genereres.";
 	}
